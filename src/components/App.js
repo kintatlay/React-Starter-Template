6f8fc9e4849a551987dfb813807e9e042e030c4a
Kin Lay <kinlay@Kins-MacBook-Pro.local>
2020-06-12 05:35:28 -0700

    completed template

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles/style.css';
+import './styles/style.scss';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <p className="text-color">Hello</p>
+            </div>
+        )
+    }
+}
+
+export default App;
